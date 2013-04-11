3a0a8524676f49a86d450ba8be4937ba9eef41b0
whatot <whatot2@gmail.com>
2013-04-11 21:08:33 +0800

    finish the implement of bitree

--- bitree/bitree.h
+++ bitree/bitree.h
@@ -24,6 +24,7 @@ void bitree_init(BiTree *tree, void (*destroy)(void *data));
 void bitree_destory(BiTree *tree);
 int bitree_ins_left(BiTree *tree, BiTreeNode *node, const void *data);
 int bitree_ins_right(BiTree *tree, BiTreeNode *node, const void *data);
+/* bitree_rem_left ,if *node == NULL, remove all nodes. */
 void bitree_rem_left(BiTree *tree, BiTreeNode *node);
 void bitree_rem_right(BiTree *tree, BiTreeNode *node);
 /* bitree_merge, *data-->root of *merge ,success 0, fail -1. */
