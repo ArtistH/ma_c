8be67964fff8c432a45f944f0fcea3a24086065e
whatot <whatot2@gmail.com>
2013-04-14 08:28:19 +0800

    remove useless comment

--- temp/temp-str.c
+++ temp/temp-str.c
@@ -1,20 +1,3 @@
-/*
- * =====================================================================================
- *
- *       Filename:  temp-str.c
- *
- *    Description:  
- *
- *        Version:  1.0
- *        Created:  2013年04月13日 22时45分21秒
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  miragehuang (), 
- *   Organization:  
- *
- * =====================================================================================
- */
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
