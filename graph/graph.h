e1aaad69cdba0b1635c6c26f3abb5f7ddb0b1037
whatot <whatot2@gmail.com>
2013-04-15 21:13:53 +0800

    finish the implement of graph using adjacency list

--- graph/graph.h
+++ graph/graph.h
@@ -39,7 +39,7 @@ int graph_ins_edge(Graph *graph, const void *data1, const void *data2);
 int graph_rem_vertex(Graph *graph, void **data);
 int graph_rem_edge(Graph *graph, void *data1, void **data2);
 int graph_adjlist(const Graph *graph, const void *data, AdjList **adjlist);
-int graph_is_adjlist(const Graph *graph, const void *data1, const void *data2);
+int graph_is_adjacent(const Graph *graph, const void *data1, const void *data2);
 
 #define graph_adjlists(graph) ((graph)->adjlist)
 #define graph_vcount(graph) ((graph)->vcount)
