78eab6bd04221810f5ba9b0e4dfda3ad6dd02e31
whatot <whatot2@gmail.com>
2013-08-21 11:15:18 +0800

    finish ex-2.c for cover.c but [valgrind failed]

--- 3/examples/set/ex-2.c
+++ 3/examples/set/ex-2.c
@@ -24,9 +24,10 @@ static void print_skills(const Set *skills) {
 	while (i < size) {
 		data = list_data(member);
 		fprintf(stdout, "skills[%03d]=%c\n", i, *data);
+		member = list_next(member);
 		i++;
 	}
-	return;
+	fprintf(stdout, "\n");
 }
 
 /* print_players */
@@ -41,6 +42,7 @@ static void print_players(const Set *players) {
 				*(char *)((KSet *)list_data(member))->key);
 		print_skills(&((KSet *)list_data(member))->set);
 	}
+	fprintf(stdout, "\n");
 }
 
 /* match_key */
@@ -66,5 +68,167 @@ int match_skill(const void *skill1, const void *skill2) {
 
 /* main */
 int main() {
+	Set skills;
+	Set players;
+	Set covering;
+	char skills_array[SKILLS_COUNT];
+	char subids_array[PLAYER_COUNT];
+	KSet player_array[PLAYER_COUNT];
+	int retval;
+	int i;
+
+	/* Create the set of skills. */
+	fprintf(stdout, "Creating the set of skills\n");
+	set_init(&skills, match_skill, NULL);
+
+	skills_array[0] = 'a';
+	skills_array[1] = 'b';
+	skills_array[2] = 'c';
+	skills_array[3] = 'd';
+	skills_array[4] = 'e';
+	skills_array[5] = 'f';
+	skills_array[6] = 'g';
+	skills_array[7] = 'h';
+	skills_array[8] = 'i';
+	skills_array[9] = 'j';
+	skills_array[10] = 'k';
+	skills_array[11] = 'l';
+
+	for (i = 0; i < SKILLS_COUNT; i++) {
+		if (set_insert(&skills, &skills_array[i]) != 0) {
+			return 1;
+		}
+	}
+
+	print_skills(&skills);
+
+	/* Create some player sets with certain skills. */
+	fprintf(stdout, "Creating the player subsets\n");
+	set_init(&players, match_key, NULL);
+
+	subids_array[0] = '1';
+	subids_array[1] = '2';
+	subids_array[2] = '3';
+	subids_array[3] = '4';
+	subids_array[4] = '5';
+	subids_array[5] = '6';
+	subids_array[6] = '7';
+	subids_array[7] = '8';
+
+	for (i = 0; i < PLAYER_COUNT; i++) {
+		player_array[i].key = &subids_array[i];
+		set_init(&player_array[i].set, match_skill, NULL);
+
+		switch (i) {
+		case 0:
+			if (set_insert(&player_array[i].set, &skills_array[0]) != 0) {
+				return 1;
+			}
+			if (set_insert(&player_array[i].set, &skills_array[1]) != 0) {
+				return 1;
+			}
+			if (set_insert(&player_array[i].set, &skills_array[2]) != 0) {
+				return 1;
+			}
+			if (set_insert(&player_array[i].set, &skills_array[3]) != 0) {
+				return 1;
+			}
+			break;
+		case 1:
+			if (set_insert(&player_array[i].set, &skills_array[4]) != 0) {
+				return 1;
+			}
+			if (set_insert(&player_array[i].set, &skills_array[5]) != 0) {
+				return 1;
+			}
+			if (set_insert(&player_array[i].set, &skills_array[6]) != 0) {
+				return 1;
+			}
+			if (set_insert(&player_array[i].set, &skills_array[7]) != 0) {
+				return 1;
+			}
+			if (set_insert(&player_array[i].set, &skills_array[8]) != 0) {
+				return 1;
+			}
+			break;
+		case 2:
+			if (set_insert(&player_array[i].set, &skills_array[9]) != 0) {
+				return 1;
+			}
+			if (set_insert(&player_array[i].set, &skills_array[10]) != 0) {
+				return 1;
+			}
+			if (set_insert(&player_array[i].set, &skills_array[11]) != 0) {
+				return 1;
+			}
+			break;
+		case 3:
+			if (set_insert(&player_array[i].set, &skills_array[0]) != 0) {
+				return 1;
+			}
+			if (set_insert(&player_array[i].set, &skills_array[4]) != 0) {
+				return 1;
+			}
+			break;
+		case 4:
+			if (set_insert(&player_array[i].set, &skills_array[1]) != 0) {
+				return 1;
+			}
+			if (set_insert(&player_array[i].set, &skills_array[5]) != 0) {
+				return 1;
+			}
+			if (set_insert(&player_array[i].set, &skills_array[6]) != 0) {
+				return 1;
+			}
+			break;
+		case 5:
+			if (set_insert(&player_array[i].set, &skills_array[2]) != 0) {
+				return 1;
+			}
+			if (set_insert(&player_array[i].set, &skills_array[3]) != 0) {
+				return 1;
+			}
+			if (set_insert(&player_array[i].set, &skills_array[6]) != 0) {
+				return 1;
+			}
+			if (set_insert(&player_array[i].set, &skills_array[7]) != 0) {
+				return 1;
+			}
+			if (set_insert(&player_array[i].set, &skills_array[10]) != 0) {
+				return 1;
+			}
+			if (set_insert(&player_array[i].set, &skills_array[11]) != 0) {
+				return 1;
+			}
+			break;
+		default:
+			if (set_insert(&player_array[i].set, &skills_array[11]) != 0) {
+				return 1;
+			}
+		}
+		
+		if (set_insert(&players, &player_array[i]) != 0) {
+			return 1;
+		}
+	}
+	print_players(&players);
+
+	/* Compute the set covering. */
+	fprintf(stdout, "Generating the covering\n");
+	retval = cover(&skills, &players, &covering);
+	if (retval != 0) {
+		fprintf(stderr, "The set could not be covered\n");
+		return 1;
+	} else {
+		print_players(&covering);
+	}
+
+	/* Destroy the sets. */
+	fprintf(stdout, "Destroying the sets\n");
+	for (i = 0; i < PLAYER_COUNT; i++) {
+		set_destroy(&player_array[i].set);
+	}
+	set_destroy(&skills);
+
 	return 0;
 }
