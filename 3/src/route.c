26d69ad5d438a26f2667e8a970b662d4b4826b11
whatot <whatot2@gmail.com>
2013-09-02 00:41:29 +0800

    finish route.c and ex-2.c for it, valgrind success

---
+++ 3/src/route.c
@@ -0,0 +1,56 @@
+/* route.c */
+#include <stdlib.h>
+#include "graphalg.h"
+#include "list.h"
+#include "route.h"
+
+/* route */
+int route(List *paths, PathVertex *destination, PathVertex **next,
+		  int (*match)(const void *key1, const void *key2)) {
+	PathVertex *temp = NULL;
+	PathVertex *parent = NULL;
+	ListElmt *element = NULL;
+	int found;
+
+	/* Locate the destination in the list of grateways. */
+	found = 0;
+	
+	for (element = list_head(paths);
+		 element != NULL;
+		 element = list_next(element)) {
+		if (match(list_data(element), destination)) {
+			temp = list_data(element);
+			parent = ((PathVertex *)list_data(element))->parent;
+			found = 1;
+			break;
+		}
+	}
+
+	/* Return if the destination is not reachable. */
+	if (!found) {
+		return -1;
+	}
+
+	/* Compute the next gateway in the shortest path to the destination. */
+	while (parent != NULL) {
+		temp = list_data(element);
+		found = 0;
+		for (element = list_head(paths);
+			 element != NULL;
+			 element = list_next(element)) {
+			if (match(list_data(element), parent)) {
+				parent = ((PathVertex *)list_data(element))->parent;
+				found = 1;
+				break;
+			}
+		}
+
+		/* Return if the destination is not reachable. */
+		if (!found) {
+			return -1;
+		}
+	}
+	*next = temp;
+
+	return 0;
+}
