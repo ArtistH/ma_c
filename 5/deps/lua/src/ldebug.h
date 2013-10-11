c07878f8d0d0ef9a0f7161fb2a3ff4bddcf19a96
whatot <whatot2@gmail.com>
2013-10-12 01:05:44 +0800

    add more headers in lua

---
+++ 5/deps/lua/src/ldebug.h
@@ -0,0 +1,12 @@
+/*
+** $Id: ldebug.h,v 2.3.1.1 2007/12/27 13:02:25 roberto Exp $
+** Auxiliary functions from Debug Interface module
+** See Copyright Notice in lua.h
+*/
+
+#ifndef ldebug_h
+#define ldebug_h
+
+
+#include "lstate.h"
+#endif
---
+++ 5/deps/lua/src/lmem.c
@@ -0,0 +1,13 @@
+/*
+** $Id: lmem.c,v 1.70.1.1 2007/12/27 13:02:25 roberto Exp $
+** Interface to Memory Manager
+** See Copyright Notice in lua.h
+*/
+
+
+#include <stddef.h>
+
+#define lmem_c
+#define LUA_CORE
+
+#include "lua.h"
