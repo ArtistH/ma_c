817bcf39ff036e68b9f13011535f727e26475ade
whatot <whatot2@gmail.com>
2013-10-28 14:15:04 +0800

    finish lundump.c for the next lapi.c

--- 5/deps/lua/src/lundump.c
+++ 5/deps/lua/src/lundump.c
@@ -162,3 +162,73 @@ static void LoadDebug(LoadState* S, Proto* f) {
 	}
 }
 
+static Proto* LoadFunction(LoadState* S, TString* p) {
+	Proto* f;
+	if (++S->L->nCcalls > LUAI_MAXCCALLS) {
+		error(S, "code too deep");
+	}
+	f = luaF_newproto(S->L);
+	setptvalue2s(S->L, S->L->top, f);
+	incr_top(S->L);
+	f->source = LoadString(S);
+	if (f->source == NULL) {
+		f->source = p;
+	}
+	f->linedefined = LoadInt(S);
+	f->lastlinedefined = LoadInt(S);
+	f->nups = LoadByte(S);
+	f->numparams = LoadByte(S);
+	f->is_vararg = LoadByte(S);
+	f->maxstacksize = LoadByte(S);
+	LoadCode(S, f);
+	LoadConstants(S, f);
+	LoadDebug(S, f);
+	IF (!luaG_checkcode(f), "bad code");
+	S->L->top--;
+	S->L->nCcalls--;
+	return f;
+}
+
+static void LoadHeader(LoadState* S) {
+	char h[LUAC_HEADERSIZE];
+	char s[LUAC_HEADERSIZE];
+	luaU_header(h);
+	LoadBlock(S, s, LUAC_HEADERSIZE);
+	IF (memcmp(h, s, LUAC_HEADERSIZE) != 0, "bad header");
+}
+
+/*
+** load precompiled chunk
+*/
+Proto* luaU_undump (lua_State* L, ZIO* Z, Mbuffer* buff, const char* name) {
+	LoadState S;
+	if (*name == '@' || *name == '=') {
+		S.name = name + 1;
+	} else if (*name == LUA_SIGNATURE[0]) {
+		S.name = "binary string";
+	} else {
+		S.name = name;
+	}
+	S.L = L;
+	S.Z = Z;
+	S.b = buff;
+	LoadHeader(&S);
+	return LoadFunction(&S, luaS_newliteral(L, "=?"));
+}
+
+/*
+** make header
+*/
+void luaU_header(char* h) {
+	int x = 1;
+	memcpy(h, LUA_SIGNATURE, sizeof(LUA_SIGNATURE) - 1);
+	h += sizeof(LUA_SIGNATURE) - 1;
+	*h++ = (char)LUAC_VERSION;
+	*h++ = (char)LUAC_FORMAT;
+	*h++ = (char)*(char*)&x;  /* endianness */
+	*h++ = (char)sizeof(int);
+	*h++ = (char)sizeof(size_t);
+	*h++ = (char)sizeof(Instruction);
+	*h++ = (char)sizeof(lua_Number);
+	*h++ = (char)(((lua_Number)0.5)==0);  /* is lua_Number integral? */
+}
