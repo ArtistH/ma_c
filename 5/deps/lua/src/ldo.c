774e2a43e70a2fc1d62d18c16c4a0590a9e362f0
whatot <whatot2@gmail.com>
2013-12-13 01:32:33 +0800

    add lparser.h and start of ldo.c in 5

---
+++ 5/deps/lua/src/ldo.c
@@ -0,0 +1,31 @@
+/*
+** $Id: ldo.c,v 2.38.1.4 2012/01/18 02:27:10 roberto Exp $
+** Stack and Call structure of Lua
+** See Copyright Notice in lua.h
+*/
+
+
+#include <setjmp.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ldo_c
+#define LUA_CORE
+
+#include "lua.h"
+
+#include "ldebug.h"
+#include "ldo.h"
+#include "lfunc.h"
+#include "lgc.h"
+#include "lmem_h"
+#include "lobject.h"
+#include "lopcodes.h"
+#include "lparser.h"
+#include "lstate.h"
+#include "lstring.h"
+#include "ltable.h"
+#include "ltm.h"
+#include "lundump.h"
+#include "lvm.h"
+#include "lzio.h"
