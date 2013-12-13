27505c9e14b47dbe30622a96006f6f1e697e0947
whatot <whatot2@gmail.com>
2013-12-14 01:40:47 +0800

    add ldo.c in 5

--- 5/deps/lua/src/ldo.c
+++ 5/deps/lua/src/ldo.c
@@ -18,7 +18,7 @@
 #include "ldo.h"
 #include "lfunc.h"
 #include "lgc.h"
-#include "lmem_h"
+#include "lmem.h"
 #include "lobject.h"
 #include "lopcodes.h"
 #include "lparser.h"
@@ -29,3 +29,97 @@
 #include "lundump.h"
 #include "lvm.h"
 #include "lzio.h"
+
+
+
+
+/*
+** {======================================================
+** Error-recovery functions
+** =======================================================
+*/
+
+
+/* chain list of long jump buffers */
+struct lua_longjmp {
+	struct lua_longjmp *previous;
+	luai_jmpbuf b;
+	volatile int status;  /* error code */
+};
+
+
+void luaD_seterrorobj (lua_State *L, int errcode, StkId oldtop) {
+	switch (errcode) {
+	case LUA_ERRMEM:
+		{
+			setsvalue2s(L, oldtop, luaS_newliteral(L, MEMERRMSG));
+			break;
+		}
+	case LUA_ERRERR:
+		{
+			setsvalue2s(L, oldtop, luaS_newliteral(L, "error in error handing"));
+			break;
+		}
+	case LUA_ERRSYNTAX:
+	case LUA_ERRRUN:
+		{
+			setobj2s(L, oldtop, L->top - 1);
+			/* error message on current top */
+			break;
+		}
+	}
+	L->top = oldtop + 1;
+}
+
+
+static void restore_stack_limit (lua_State *L) {
+	lua_assert(L->stack_last - L->stack == L->stacksize - EXTRA_STACK - 1);
+	if (L->size_ci > LUAI_MAXCALLS) {  /* there was an overflow? */
+		int inuse = cast_int(L->ci - L->base_ci);
+		if (inuse + 1 < LUAI_MAXCALLS) {  /* can `undo' overflow? */
+			luaD_reallocCI(L, LUAI_MAXCALLS);
+		}
+	}
+}
+
+
+static void resetstack (lua_State *L, int status) {
+	L->ci = L->base_ci;
+	L->base = L->ci->base;
+	luaF_close(L, L->base);  /* close eventual pending closures */
+	luaD_seterrorobj(L, status, L->base);
+	L->nCcalls = L->baseCcalls;
+	L->allowhook = 1;
+	restore_stack_limit(L);
+	L->errfunc = 0;
+	L->errorJmp = NULL;
+}
+
+
+void luaD_throw (lua_State *L, int errcode) {
+	if (L->errorJmp) {
+		L->errorJmp->status = errcode;
+		LUAI_THROW(L, L->errorJmp);
+	} else {
+		L->status = cast_byte(errcode);
+		if (G(L)->panic) {
+			resetstack(L, errcode);
+			lua_unlock(L);
+			G(L)->panic(L);
+		}
+		exit(EXIT_FAILURE);
+	}
+}
+
+
+int luaD_rawrunprotected (lua_State *L, Pfunc f, void *ud) {
+	struct lua_longjmp lj;
+	lj.status = 0;
+	lj.previous = L->errorJmp;  /* chain new error handler */
+	L->errorJmp = &lj;
+	LUAI_TRY(L, &lj,
+			 (*f)(L, ud);
+			 );
+	L->errorJmp = lj.previous;  /* restore old error handler */
+	return lj.status;
+}
